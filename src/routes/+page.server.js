1521a5ba2f35d4bbedf6a9d596ac3ba99931ee3d
Tiago Araujo <tiagomuniz130@gmail.com>
2025-06-30 06:46:18 -0300

    feat: add a simple cache mechanism to avoid rate limit

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,3 +1,9 @@
+// This is a simple caching mechanism to avoid exciding the API rate limits
+// In a real cenario, would be better to use a more robut caching solution like Redis
+const cache = new Map();
+const CACHE_TTL = 1000 * 60 * 60; // 1 hour in milliseconds
+const MAX_CACHE_SIZE = 100; // To avoid memory bloat if a logt of unique names are queried
+
 export async function load({ url }) {
 	const name = url.searchParams.get('name');
 	if (!name) {
@@ -8,10 +14,39 @@ export async function load({ url }) {
 		};
 	}
 
+	if (cache.has(name)) {
+		const cachedData = cache.get(name);
+		const now = Date.now();
+
+		// Move the last accessed iten to the end of the Map.
+		cache.delete(name);
+		cache.set(name, cachedData);
+
+		if (now - cachedData.timestamp < CACHE_TTL) {
+			return cachedData.data;
+		} else {
+			cache.delete(name);
+		}
+	}
+
 	try {
 		const res = await fetch(`https://api.agify.io?name=${name}`);
 		if (res.ok) {
 			const data = await res.json();
+
+			if (cache.size >= MAX_CACHE_SIZE) {
+				const leastUsedKey = cache.keys().next().value;
+				cache.delete(leastUsedKey);
+			}
+			cache.set(name, {
+				data: {
+					name: data.name,
+					age: data.age,
+					count: data.count
+				},
+				timestamp: Date.now()
+			});
+
 			return {
 				name: data.name,
 				age: data.age,
